57a7af8da70439802714dc0236bcc745afa421a2
Nguyễn Trọng Nhân <nhantrong3123@gmail.com>
2021-10-13 12:24:33 +0700

    add funtion Tinh_n, Tinh_sn

--- RSA-Cryptool/RSA-Cryptool/Form1.Designer.cs
+++ RSA-Cryptool/RSA-Cryptool/Form1.Designer.cs
@@ -33,16 +33,19 @@ namespace RSA_Cryptool
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.plainTextBox = new System.Windows.Forms.RichTextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.CypherTextBox = new System.Windows.Forms.RichTextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.button_Crypt = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -51,46 +54,48 @@ namespace RSA_Cryptool
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(354, 198);
+            this.textBox2.Location = new System.Drawing.Point(466, 198);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 0;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(137, 201);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.Size = new System.Drawing.Size(15, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "p";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(323, 201);
+            this.label2.Location = new System.Drawing.Point(435, 201);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.Size = new System.Drawing.Size(15, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "q";
             // 
-            // richTextBox1
+            // plainTextBox
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(140, 86);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(426, 96);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
+            this.plainTextBox.Location = new System.Drawing.Point(140, 86);
+            this.plainTextBox.Name = "plainTextBox";
+            this.plainTextBox.Size = new System.Drawing.Size(426, 96);
+            this.plainTextBox.TabIndex = 2;
+            this.plainTextBox.Text = "";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(47, 115);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.Size = new System.Drawing.Size(62, 16);
             this.label3.TabIndex = 3;
             this.label3.Text = "plainText";
             // 
@@ -103,7 +108,7 @@ namespace RSA_Cryptool
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(354, 254);
+            this.textBox4.Location = new System.Drawing.Point(466, 254);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 22);
             this.textBox4.TabIndex = 0;
@@ -113,65 +118,102 @@ namespace RSA_Cryptool
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(137, 257);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.Size = new System.Drawing.Size(14, 16);
             this.label4.TabIndex = 1;
             this.label4.Text = "n";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(323, 257);
+            this.label5.Location = new System.Drawing.Point(435, 257);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(16, 17);
+            this.label5.Size = new System.Drawing.Size(15, 16);
             this.label5.TabIndex = 1;
             this.label5.Text = "d";
             // 
-            // richTextBox2
+            // CypherTextBox
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(140, 309);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(426, 96);
-            this.richTextBox2.TabIndex = 2;
-            this.richTextBox2.Text = "";
+            this.CypherTextBox.Location = new System.Drawing.Point(140, 309);
+            this.CypherTextBox.Name = "CypherTextBox";
+            this.CypherTextBox.Size = new System.Drawing.Size(426, 96);
+            this.CypherTextBox.TabIndex = 2;
+            this.CypherTextBox.Text = "";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(47, 338);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(80, 17);
+            this.label6.Size = new System.Drawing.Size(76, 16);
             this.label6.TabIndex = 3;
             this.label6.Text = "CypherText";
             // 
-            // button1
+            // button_Crypt
             // 
-            this.button1.Location = new System.Drawing.Point(533, 257);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(79, 28);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Crypt";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button_Crypt.Location = new System.Drawing.Point(621, 351);
+            this.button_Crypt.Name = "button_Crypt";
+            this.button_Crypt.Size = new System.Drawing.Size(79, 28);
+            this.button_Crypt.TabIndex = 4;
+            this.button_Crypt.Text = "Crypt";
+            this.button_Crypt.UseVisualStyleBackColor = true;
+            this.button_Crypt.Click += new System.EventHandler(this.button_Crypt_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(63, 24);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(58, 17);
+            this.label7.Size = new System.Drawing.Size(55, 16);
             this.label7.TabIndex = 5;
             this.label7.Text = "ENcrypt";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(276, 201);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(146, 16);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "This fill cannot be blank";
+            this.label8.Visible = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(572, 201);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(146, 16);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "This fill cannot be blank";
+            this.label9.Visible = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(572, 115);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(146, 16);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "This fill cannot be blank";
+            this.label10.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_Crypt);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.CypherTextBox);
+            this.Controls.Add(this.plainTextBox);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label4);
@@ -181,6 +223,7 @@ namespace RSA_Cryptool
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
+            this.Text = "Encryption";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -192,16 +235,19 @@ namespace RSA_Cryptool
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox plainTextBox;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox CypherTextBox;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_Crypt;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
 
--- RSA-Cryptool/RSA-Cryptool/Form1.cs
+++ RSA-Cryptool/RSA-Cryptool/Form1.cs
@@ -12,9 +12,75 @@ namespace RSA_Cryptool
 {
     public partial class Form1 : Form
     {
+        string M;
+        List<char> C;
+        int p, q, X;
+        double n, sn, d;
+
+        private void button_Crypt_Click(object sender, EventArgs e)
+        {
+            if (plainTextBox.Text != "") label10.Visible = false; else label10.Visible = true;
+            if (textBox1 != null) label8.Visible = false; else label8.Visible = true;
+            if (textBox2 != null) label9.Visible = false; else label9.Visible = true;
+            
+            if (label8 != null && label9 != null && label10 != null) 
+            {
+                EXtract_form_TxtBox();
+                n = Tinh_n(); 
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            textBox3.ReadOnly = true;
+            textBox4.ReadOnly = true;
         }
+        
+        public List<char> toASCII(string aString) //pending...Txt to ASCII
+        {
+            C[0] = 'q';  
+            return C;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e) //number only
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)){
+                e.Handled = true;   
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e) //number only
+        {
+            if(!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        public void EXtract_form_TxtBox()  //take information from textBox
+        {
+            M = plainTextBox.Text.Trim();
+            p = Int32.Parse(textBox1.Text);
+            q = Int32.Parse(textBox2.Text);
+        }
+        public double Tinh_n() //cal n
+        {
+            double c = (double)p*q;
+            textBox3.Text = c.ToString();
+            return n;
+        }
+        public double Tinh_sn()  //cal sn
+        {
+            double d = (double)(p - 1) * (q - 1);
+            return sn;
+        }
+        public int Tim_X() //find X
+        {
+
+            return X;
+        }
+        
+        
     }
 }
